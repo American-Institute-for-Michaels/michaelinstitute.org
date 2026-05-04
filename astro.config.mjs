c070a9a91f0939d86e2a5d78eb238b8d05821978
Michael Doyle <michaeldoyle1994@gmail.com>
2026-05-04 08:25:37 -0700

    feat: add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://michaelinstitute.org',
@@ -7,4 +8,5 @@ export default defineConfig({
   build: {
     format: 'file',
   },
+  integrations: [sitemap()],
 });
